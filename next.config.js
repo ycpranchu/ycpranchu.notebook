94e4f37df81dafd232ffaf175aaa3da9175e0aed
ycpin <yangpokemonpin@gmail.com>
2023-09-16 14:46:41 +0800

    testing_3

--- next.config.js
+++ next.config.js
@@ -2,29 +2,23 @@
  * @type {import('next').NextConfig}
  **/
 
+const isProduction = process.env.NODE_ENV === "production";
+const assetPrefix = isProduction ? "/ycpranchu.github.io" : "";
+
 const withNextra = require('nextra')({
     theme: 'nextra-theme-docs',
     themeConfig: './theme.config.jsx',
     defaultShowCopyCode: true,
     latex: true,
     staticImage: true,
-})
 
-const isProduction = process.env.NODE_ENV === "production";
-const assetPrefix = isProduction ? "/ycpranchu.github.io" : "";
-
-const nextConfig = {
     images: {
         unoptimized: true,
     },
     reactStrictMode: true,
     swcMinify: true,
     trailingSlash: true,
-    assetPrefix,
     basePath: assetPrefix,
-};
+});
 
-module.exports = {
-    ...withNextra(),
-    ...nextConfig,
-};
+module.exports = withNextra();
