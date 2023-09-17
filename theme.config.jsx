c1ea04f1b9f223c8fa2a139cd777ee30b7f146bd
ycpin <yangpokemonpin@gmail.com>
2023-09-17 21:18:38 +0800

    page update

--- theme.config.jsx
+++ theme.config.jsx
@@ -11,7 +11,7 @@ export default {
     project: {
         link: 'https://github.com/ycpranchu'
     },
-    docsRepositoryBase: 'https://github.com/ycpranchu/ycpranchu.notebook',
+    docsRepositoryBase: 'https://github.com/ycpranchu/ycpranchu.notebook/tree/main',
     useNextSeoProps() {
         const { asPath } = useRouter()
         if (asPath !== '/') {
